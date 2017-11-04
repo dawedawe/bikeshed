fa490eecb3f6209c0eaa468c460b7c5bc7f5c6d6
dawe <dawedawe@gmx.de>
2017-11-04 16:31:12 +0100

    add some more tests

--- BikeShed.test/LogicTests.fs
+++ BikeShed.test/LogicTests.fs
@@ -12,10 +12,16 @@ module LogicTests =
     let ``adding a bike works`` () =
         let bikes1 = getBikes()
         let bike = { Name = "testbike"; Color = "green" }
-        match createBike bike with
-        | Success b -> Assert.Same(b.Name, bike.Name)
-                       Assert.Same(b.Color, bike.Color)
-        | Failure _ -> Assert.True(false)
+        createBike bike
+        |> function
+           | Success b -> Assert.Same(b.Name, bike.Name)
+                          Assert.Same(b.Color, bike.Color)
+           | Failure _ -> Assert.True(false)
+        getBike bike.Name
+        |> function
+           | Success b -> Assert.Same(b.Name, bike.Name)
+                          Assert.Same(b.Color, bike.Color)
+           | Failure _ -> Assert.True(false)
         let bikes2 = getBikes()
         Assert.True(bikes1.Where(fun b -> b.Name = bike.Name).Count() = 0)
         Assert.True(bikes2.Where(fun b -> b.Name = bike.Name).Count() = 1)
@@ -26,8 +32,43 @@ module LogicTests =
         let bikes1 = getBikes()
         let bike = { Name = "testbike"; Color = "green" }
         createBike bike |> ignore
-        match deleteBike bike.Name with
-        | Success b -> Assert.Same(b, bike.Name)
-        | Failure _ -> Assert.True(false)
+        deleteBike bike.Name
+        |> function
+           | Success b -> Assert.Same(b, bike.Name)
+           | Failure _ -> Assert.True(false)
         let bikes2 = getBikes()
         Assert.Equal(bikes1.Count, bikes2.Count)
+
+    [<Fact>]
+    let ``deleting unknown bike fails`` () =
+        let bikes1 = getBikes()
+        deleteBike "unknownbike"
+        |> function
+           | Success _ -> Assert.True(false)
+           | Failure _ -> Assert.True(true)
+        let bikes2 = getBikes()
+        Assert.Equal(bikes1.Count, bikes2.Count)
+
+
+    [<Fact>]
+    let ``changing a bike works`` () =
+        let bike = { Name = "testbike"; Color = "green" }
+        createBike bike |> ignore
+        let changedBike = { bike with Color = "red" }
+        updateBike changedBike.Name changedBike
+        |> function
+           | Success b -> Assert.Same(b.Name, changedBike.Name)
+                          Assert.Same(b.Color, changedBike.Color)
+           | Failure _ -> Assert.True(false)
+
+
+    [<Fact>]
+    let ``changing a bikes name fails`` () =
+        let bike = { Name = "testbike"; Color = "green" }
+        createBike bike |> ignore
+        let changedBike = { bike with Name = "changedname" }
+        updateBike changedBike.Name changedBike
+        |> function
+           | Success _ -> Assert.True(false)
+           | Failure _ -> Assert.True(true)
+
